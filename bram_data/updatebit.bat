0cf919d1b6412eaf69e576964da927c0e213db87
Ryan Clarke <kj6msg@icloud.com>
2018-08-12 11:28:53 -0500

    Moved BRAM init files into "./bram_data/".
    BRAM initialization files (.COE, .MEM, and .MMI) were moved to their
    own directory, "./bram_data/". The updatebit.bat script was also moved
    to the directory. The makefile was updated to reflect the changes.

--- bram_data/updatebit.bat
+++ bram_data/updatebit.bat
@@ -5,7 +5,7 @@ set PATH=%PROJDIR%;C:\Xilinx\Vivado\2017.2\bin;%PATH%
 
 set MMIFILE=bios.mmi
 set MEMFILE=bios.mem
-set BITFILE=..\..\v65C02.runs\impl_1\v65C02_Top.bit
-set OUTFILE=..\..\v65C02.runs\impl_1\v65C02_Top_updated.bit
+set BITFILE=..\v65C02.runs\impl_1\v65C02_Top.bit
+set OUTFILE=..\v65C02.runs\impl_1\v65C02_Top_updated.bit
 
 updatemem --force --meminfo %MMIFILE% --data %MEMFILE% --bit %BITFILE% --proc dummy --out %OUTFILE%
